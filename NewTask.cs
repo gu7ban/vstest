335a7de631610b609f5ac1d5c1597c6f65b47e7e
gu7ban <montanatony765lll@gmail.com>
2025-10-29 18:42:33 +0400

    Yeni tasklar

---
+++ NewTask.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public static class ExtensionMethods
+{
+    public static bool IsOdd(this int number)
+    {
+        return number % 2 != 0;
+    }
+    public static bool IsEven(this int number)
+    {
+        return number % 2 == 0;
+    }
+    public static bool IsContainsDigit(this string str)
+    {
+        foreach (char c in str)
+        {
+            if (char.IsDigit(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static string ToCapitalize(this string str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return str;
+
+        return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+    }
+
+    public static int[] GetValueIndexes(this string str, char value)
+    {
+        int count = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == value)
+            {
+                count++;
+            }
+        }
+        if (count == 0) return new int[0];
+        int[] indexes = new int[count];
+        int index = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == value)
+            {
+                indexes[index++] = i;
+            }
+        }
+
+        return indexes;
+    }
+}
+
+public class NewTask
+{
+    public static void Main()
+    {
+        int number = 5;
+        Console.WriteLine($"{number} is odd: {number.IsOdd()}");
+        Console.WriteLine($"{number} is even: {number.IsEven()}");
+
+        string text = "Hello123";
+        Console.WriteLine($"Does '{text}' contain digits: {text.IsContainsDigit()}");
+
+        string name = "john";
+        Console.WriteLine($"Capitalized: {name.ToCapitalize()}");
+
+        string sentence = "Hello, world!";
+        char searchChar = 'o';
+        int[] indexes = sentence.GetValueIndexes(searchChar);
+        Console.WriteLine($"Indexes of '{searchChar}' in '{sentence}': {string.Join(", ", indexes)}");
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public interface IAccount
+{
+    bool PasswordChecker(string password);
+    void ShowInfo();
+}
+public class User : IAccount
+{
+    private static int _idCounter = 4;
+    private string _password;
+    public readonly int Id;
+    public string Fullname { get; set; }
+    public string Email { get; set; }
+    public string Password
+    {
+        set
+        {
+            if (PasswordChecker(value))
+            {
+                _password = value;
+            }
+            else
+            {
+                throw new ArgumentException("Sifre sertlere uygun deyil.");
+            }
+        }
+    }
+    public User(string email,string password)
+    {
+        Id = _idCounter++;
+        Email = email;
+        Password = password;
+    }
+public bool PasswordChecker(string password)
+    {
+        if(password.Length<8)
+            return false;
+        if (!password.Any(char.IsUpper))
+            return false;
+        if(!password.Any(char.IsLower))
+            return false;
+        if (!password.Any(char.IsDigit))
+            return false;
+
+        return true;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine("Id: "+Id);
+        Console.WriteLine("Fullname: "+Fullname);
+        Console.WriteLine("Email: "+Email);
+    }
+}
+
+public class Task2
+{
+    public static void Main()
+    {
+        try
+        {
+            User user1 = new User("mikayilehmedov@gmail.com", "Salam123");
+            user1.Fullname = "Mikayil Ehmedov";
+            user1.ShowInfo();
+
+            User user2 = new User("malikzulfuqarov@gmail.com", "pass");
+        }
+        catch(ArgumentException ex)
+        {
+            Console.WriteLine("Xeta: " + ex.Message);
+        }
+    }
+}
